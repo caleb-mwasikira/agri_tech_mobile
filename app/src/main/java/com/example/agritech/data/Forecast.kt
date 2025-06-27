42ecf876edc74ea294fb842cd0065c6545822095
Caleb Mwas <calebmwasikira@gmail.com>
2025-06-27 22:25:20 +0300

    Initialised new project. AgriTech mobile platform

---
+++ app/src/main/java/com/example/agritech/data/Forecast.kt
@@ -0,0 +1,48 @@
+package com.example.agritech.data
+
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Immutable
+import com.example.agritech.R
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+fun getCurrentDateTime(): String {
+    val currentDateTime = LocalDateTime.now()
+    val formatter = DateTimeFormatter.ofPattern("dd MMM uuuu")
+    return currentDateTime.format(formatter)
+}
+
+@Immutable
+data class Note(
+    val id: Int,
+    val title: String,
+    val description: String,
+    @DrawableRes val imageUrl: Int,
+)
+
+val notes = mutableListOf(
+    Note(
+        id = 1,
+        title = "From Seed to Sip. The Journey of a Tea Plant",
+        description = "Explore the life cycle of a tea plant, from careful seed selection and nursery practices to the first flush of leaves ready for harvest.",
+        imageUrl = R.drawable.tea
+    ),
+    Note(
+        id = 2,
+        title = "How Shade-Grown Coffee Enhances Flavor and Sustainability",
+        description = "Discover why shade-grown coffee farms are becoming more popular and how they benefit both flavor quality and the environment",
+        imageUrl = R.drawable.coffee_beans,
+    ),
+    Note(
+        id = 3,
+        title = "The Art of Hand-Harvesting Tea",
+        description = "Go behind the scenes with tea pickers and learn how traditional hand-plucking ensures only the finest leaves make it to your cup",
+        imageUrl = R.drawable.tea_harvesting,
+    ),
+    Note(
+        id = 4,
+        title = "Drying Beds and Sun-Kissed Beans",
+        description = "Learn how traditional sun-drying methods on raised beds help preserve the rich flavor of freshly harvested coffee beans",
+        imageUrl = R.drawable.coffee_beans_2,
+    )
+)
---
+++ app/src/main/java/com/example/agritech/ui/theme/Type.kt
@@ -0,0 +1,40 @@
+package com.example.agritech.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.agritech.R
+
+val OutfitFont = FontFamily(
+    Font(R.font.outfit_regular),
+    Font(R.font.outfit_bold, weight = FontWeight.Bold),
+    Font(R.font.outfit_semibold, weight = FontWeight.SemiBold),
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = OutfitFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = OutfitFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = OutfitFont,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+)
